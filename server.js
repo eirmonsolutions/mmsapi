51a1bd45c548a9a977b113504bdf35ed7155df31
Vishal Thakur <157201590+vishalthakur0012@users.noreply.github.com>
2024-08-12 12:04:20 +0530

    node js version update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const Product = require('./productModel');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
